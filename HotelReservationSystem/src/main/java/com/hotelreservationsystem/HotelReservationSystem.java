b8b5af18d32f6d19cbfd9bda8b3d2a906d12fdb3
NayanTr09 <nayantripathi42@gmail.com>
2022-01-25 08:17:44 +0530

    Added : Welcome message

---
+++ HotelReservationSystem/src/main/java/com/hotelreservationsystem/HotelReservationSystem.java
@@ -0,0 +1,14 @@
+/*
+@author : Nayan Tripathi
+@Date : 25/01/2022
+Workshop Program : Hotel Reservation System
+ */
+
+
+package com.hotelreservationsystem;
+
+public class HotelReservationSystem {
+    public static void main(String args[]){
+        System.out.println("Welcome to Hotel Reservation System"); //Welcome message
+    }
+}
